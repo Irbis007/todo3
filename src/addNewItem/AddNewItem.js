83446cec55dad86d4eacbf20a160f9490516481e
Irbis007 <suskodomk@gmail.com>
2023-02-10 00:27:10 +0300

    simplified add new item

--- src/addNewItem/AddNewItem.js
+++ src/addNewItem/AddNewItem.js
@@ -1,201 +1,190 @@
-import { Component } from 'react'
-import {CgMathPlus} from 'react-icons/cg'
+import { Component } from "react";
+import { CgMathPlus } from "react-icons/cg";
 
-import DataList from '../listItem/DataList/DataList';
-import InputItem from '../listItem/item/Item';
+import DataList from "../listItem/DataList/DataList";
+import InputItem from "../listItem/item/Item";
 
-import './addNewItem.css'
+import "./addNewItem.css";
 
+class AddNewItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      href: "",
+      urlMarker: "",
+      title: "",
+      text: "",
+      urlImg: "",
+      lat: 0,
+      lng: 0,
 
-class AddNewItem extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            href: '',
-            urlMarker: '',
-            title: '',
-            text: '',
-            urlImg: '',
-            lat: 0,
-            lng: 0,
-            
-            links: [
-                {url: '', name: ''}
-            ]
-        }
+      links: [{ url: "", name: "" }],
+    };
+  }
+  changeValue = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  onAddItem = (e) => {
+    e.preventDefault();
+    this.props.onAddData({
+      href: this.state.href,
+      urlImgMarker: this.state.urlMarker,
+
+      bangalore: {
+        lat: +this.state.lat,
+        lng: +this.state.lng,
+      },
+      dataPopup: {
+        title: this.state.title,
+        text: this.state.text,
+        urlImg: this.state.urlImg,
+        links: this.state.links,
+      },
+    });
+    this.setState({
+      href: "",
+      urlMarker: "",
+      title: "",
+      text: "",
+      urlImg: "",
+      lat: 0,
+      lng: 0,
+      links: [{ url: "", name: "" }],
+    });
+  };
+
+  createId = () => {
+    return Math.random() * (10000 - -10000) + -10000;
+  };
+
+  getData = (value, id) => {
+    if (this.state.links) {
+      this.state.links.map((item) => {
+        if (id === item.linkId) {
+          item.url = value.url;
+          item.name = value.name;
+          return item;
+        } else return item;
+      });
     }
-    changeValue = (e) =>{
-        this.setState({[e.target.name]: e.target.value})
-    }
-    onAddItem = (e) =>{
-        e.preventDefault()
-        this.props.onAddData({
-            href: this.state.href,
-            urlImgMarker: this.state.urlMarker,
+  };
 
-            bangalore: {
-                lat: +this.state.lat,
-                lng: +this.state.lng
-            },
-            dataPopup: {
-                title: this.state.title,
-                text: this.state.text,
-                urlImg: this.state.urlImg,
-                links: this.state.links
-            }
-        })
-        this.setState({
-            href: '',
-            urlMarker: '',
-            title: '',
-            text: '',
-            urlImg: '',
-            lat: 0,
-            lng: 0,
-            links: [
-                {url: '', name: ''}
-            ]
-        })
-    }
+  onAddLinks = (e) => {
+    e.preventDefault();
+    let newArr = this.state.links;
+    let linkId = this.createId();
+    newArr.push({ url: "", name: "", linkId: `${linkId}` });
+    this.setState({ links: newArr });
+  };
+  deleteListItem = (id) => {
+    this.setState(({ links }) => {
+      return {
+        links: links.filter((item) => item.linkId !== id),
+      };
+    });
+  };
 
-    createId = () =>{
-        return Math.random() * (10000 - -10000) + -10000;
-    }
+  render() {
+    const { href, urlMarker, title, text, urlImg, lat, lng, links } =
+      this.state;
 
-    getData = (value, id) =>{
-        if(this.state.links){
-            this.state.links.map(item =>{
-                if(id === item.linkId){
-                    item.url = value.url
-                    item.name = value.name
-                    return item
-                } else return item
-            })
-        }
-    }
+    const elem = links.map((item, i) => {
+      let deleteItem;
+      let label = false;
+      if (i < 1) {
+        label = true;
+      }
+      if (i < 1) {
+        deleteItem = false;
+      } else {
+        deleteItem = true;
+      }
+      if (!item.linkId) {
+        item.linkId = `${this.createId()}`;
+      }
+      return (
+        <DataList
+          url={item.url}
+          name={item.name}
+          key={item.linkId}
+          linkId={item.linkId}
+          getData={this.getData}
+          deleteItem={deleteItem}
+          onDelete={this.deleteListItem}
+          label={label}
+        />
+      );
+    });
 
-    onAddLinks = (e) =>{
-        e.preventDefault()
-        let newArr = this.state.links
-        let linkId = this.createId()
-        newArr.push({url: '', name: '', linkId: `${linkId}`})
-        this.setState({links: newArr})
-    }
-    
-/* fsdbgnhmjgl;,hfknjbd hdjvgdjbfknglmh;,'''''' ksfn,dnt;kmehiprgbouewjsvnltnejoihprgobuelnwksm.vfnivougevwabsd */
-
-    deleteListItem = (id) =>{
-        this.setState(({links}) => {
-            return {
-                links: links.filter(item => item.linkId !== id)
-            }
-        })
-    }
-    
-
-    render() {  
-        const {href, urlMarker, title, text, urlImg, lat, lng, links} = this.state
-
-
-
-        const elem = links.map((item, i) =>{
-            let deleteItem;
-            let label =false
-            if(i< 1){
-                label = true
-            }
-            if(i < 1){
-                deleteItem = false;
-            } else {deleteItem = true}
-            if(!item.linkId){
-                item.linkId = `${this.createId()}`
-            }
-            return(
-                <DataList url={item.url} name={item.name} key={item.linkId} linkId={item.linkId} getData={this.getData} deleteItem={deleteItem} onDelete={this.deleteListItem} label={label}/>
-            )
-        })
-
-
-        return(
-            <div className="add__item">
-                <form action="" >
-                <ul className="list">
-                    <li className="item">
-                        <label htmlFor="href">Href</label>
-                        <input type="text" id="href"
-                            placeholder='href'
-                            name='href'
-                            value={href}
-                            onChange={this.changeValue}
-                        />
-                    </li>
-                    <li className="item">
-                        <label htmlFor="url-marker">Url img marker</label>
-                        <input type="text" id="url-marker"
-                            placeholder='url-marker'
-                            name='urlMarker'
-                            value={urlMarker}
-                            onChange={this.changeValue}
-                        />
-                    </li>
-                    <li className="item">
-                        <label htmlFor="title">Title</label>
-                        <input type="text" id="title"
-                            placeholder='title'
-                            name='title'
-                            value={title}
-                            onChange={this.changeValue}
-                        />
-                    </li>
-                    <li className="item">
-                        <label htmlFor="text">Text</label>
-                        <input type="text" id="text"
-                            placeholder='text'
-                            name='text'
-                            value={text}
-                            onChange={this.changeValue}
-                        />
-                    </li>
-                    <li className="item">
-                        <label htmlFor="url-img">Url img</label>
-                        <input type="text" id="url-img"
-                            placeholder='url-img'
-                            name='urlImg'
-                            value={urlImg}
-                            onChange={this.changeValue}
-                        />
-                    </li>
-                    <li className="item">
-                        <label htmlFor="lat">Lat</label>
-                        <input type="number" id="lat"
-                            placeholder='lat'
-                            name='lat'
-                            value={lat}
-                            onChange={this.changeValue}
-                        />
-                    </li>
-                    <li className="item">
-                        <label htmlFor="lng">Lng</label>
-                        <input type="number" id="lng"
-                            placeholder='lng'
-                            name='lng'
-                            value={lng}
-                            onChange={this.changeValue}
-                        />
-                    </li>
-                    <ul className="data__list">
-                        {elem}
-                    </ul>
-                    <li className='buttons__block'>
-                        <button className='add__item-button' onClick={this.onAddItem}>Save</button>
-                        <button className="add__list-links" onClick={this.onAddLinks}><CgMathPlus/></button>
-                    </li>
-                </ul>
-                </form>
-            </div>
-        )
-    }
+    return (
+      <div className="add__item">
+        <form action="">
+          <ul className="list">
+            <InputItem
+              label="Href"
+              id="href"
+              name="href"
+              onChangeValue={this.changeValue}
+              valueProp={href}
+            />
+            <InputItem
+              label="Url img marker"
+              id="url-marker"
+              name="urlMarker"
+              onChangeValue={this.changeValue}
+              valueProp={urlMarker}
+            />
+            <InputItem
+              label="Title"
+              id="title"
+              name="title"
+              onChangeValue={this.changeValue}
+              valueProp={title}
+            />
+            <InputItem
+              label="Text"
+              id="text"
+              name="text"
+              onChangeValue={this.changeValue}
+              valueProp={text}
+            />
+            <InputItem
+              label="Url img"
+              id="url-img"
+              name="urlImg"
+              onChangeValue={this.changeValue}
+              valueProp={urlImg}
+            />
+            <InputItem
+              label="Lat"
+              id="lat"
+              name="lat"
+              onChangeValue={this.changeValue}
+              valueProp={lat}
+              type="number"
+            />
+            <InputItem
+              label="Lng"
+              id="lng"
+              name="lng"
+              onChangeValue={this.changeValue}
+              valueProp={lng}
+              type="number"
+            />
+            <ul className="data__list">{elem}</ul>
+            <li className="buttons__block">
+              <button className="add__item-button" onClick={this.onAddItem}>
+                Save
+              </button>
+              <button className="add__list-links" onClick={this.onAddLinks}>
+                <CgMathPlus />
+              </button>
+            </li>
+          </ul>
+        </form>
+      </div>
+    );
+  }
 }
 
-
-export default AddNewItem
+export default AddNewItem;
